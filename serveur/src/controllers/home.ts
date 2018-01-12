87fe77daad7308ae2dde73ebbcca992266901167
Gabriel BOurgault <gabriel.bourgault@polymtl.ca>
2018-01-12 11:16:51 -0500

    refs #14723 [server] Home route send json

--- serveur/src/controllers/home.ts
+++ serveur/src/controllers/home.ts
@@ -5,7 +5,8 @@ import { Request, Response } from "express";
  * Home page.
  */
 export let index = (req: Request, res: Response) => {
-  res.render("home", {
+  res.json({ "hey": "Hey" });
+  /*res.render("home", {
     title: "Home"
-  });
+  });*/
 };
