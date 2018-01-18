f2e74f3c18084c4814d3d4fd2b23cbeffd2490f5
Frederic Halle <frederic.halle@polymtl.ca>
2018-01-18 14:28:37 -0500

    refs #14790 [leger] Travail en cours pour les constraints

--- client-leger/PolyPaintPro/PolyPaintPro/AppDelegate.swift
+++ client-leger/PolyPaintPro/PolyPaintPro/AppDelegate.swift
@@ -11,9 +11,9 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
-
+    var window: UIWindow?  
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
--- client-leger/PolyPaintPro/PolyPaintPro/ViewController.swift
+++ client-leger/PolyPaintPro/PolyPaintPro/ViewController.swift
@@ -9,9 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var landScapeConstraint: NSLayoutConstraint!
+  
+    @IBOutlet weak var portrateConstraint: NSLayoutConstraint!
+    
+    
+    
+    func viewWillTransitionToSize(size: CGSize,   withTransitionCoordinator coordinator:    UIViewControllerTransitionCoordinator) {
+        
+        coordinator.animate(alongsideTransition: { (UIViewControllerTransitionCoordinatorContext) -> Void in
+            
+            let orient = UIApplication.shared.statusBarOrientation
+            
+            switch orient {
+            case .portrait:
+                print("Portrait")
+                self.ApplyportraitConstraint()
+                break
+            // Do something
+            default:
+                print("LandScape")
+                // Do something else
+                self.applyLandScapeConstraint()
+                break
+            }
+        }, completion: { (UIViewControllerTransitionCoordinatorContext) -> Void in
+            print("rotation completed")
+        })
+        viewWillTransitionToSize(size: size, withTransitionCoordinator: coordinator)
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func ApplyportraitConstraint(){
+       
+        self.view.addConstraint(self.portrateConstraint)
+        self.view.removeConstraint(self.landScapeConstraint)
+    }
+    func applyLandScapeConstraint(){
+        
+        self.view.removeConstraint(self.portrateConstraint)
+        self.view.addConstraint(self.landScapeConstraint)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +71,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+   
 
 }
 
