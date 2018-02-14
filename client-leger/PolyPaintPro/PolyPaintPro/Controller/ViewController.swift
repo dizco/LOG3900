8eba99e031e7d8d3047dee15b590132c689e2074
Frederic Halle <frederic.halle@polymtl.ca>
2018-02-13 21:29:18 -0500

    refs #16115 [leger] implemented basic drawing functionality

--- client-leger/PolyPaintPro/PolyPaintPro/Controller/ViewController.swift
+++ client-leger/PolyPaintPro/PolyPaintPro/Controller/ViewController.swift
@@ -13,6 +13,59 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet weak var tempImageView: UIImageView!
+    var lastPoint = CGPoint.zero //last dran point on the cnvas
+    var red: CGFloat = 0.0 //RGB, stores the currend rgb value from the selector
+    var green: CGFloat = 0.0
+    var blue: CGFloat = 0.0
+    var brushWidth: CGFloat = 10.0 //bruhs stroke and opacity
+    var opacity: CGFloat = 1.0
+    var swiped = false //if the brush stroke is continuous
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = false
+        if let touch = touches.first as? UITouch {
+            lastPoint = touch.location(in: self.view)
+        }
+    }
+
+    func drawLine(fromPoint: CGPoint, toPoint: CGPoint) {
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 0)
+
+        tempImageView.image?.draw(in: view.bounds)
+
+        let context = UIGraphicsGetCurrentContext()
+
+        context?.move(to: fromPoint)
+        context?.addLine(to: toPoint)
+
+        context?.setLineCap(CGLineCap.round)
+        context?.setLineWidth(brushWidth)
+        context?.setStrokeColor(red: red, green: green, blue: blue, alpha: 1.0)
+        context?.setBlendMode(CGBlendMode.normal)
+        context?.strokePath()
+
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImageView.alpha = opacity
+        UIGraphicsEndImageContext()
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        swiped = true
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: view)
+            drawLine(fromPoint: lastPoint, toPoint: currentPoint)
+
+            lastPoint = currentPoint
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !swiped {
+            // draw a single point
+            self.drawLine(fromPoint: lastPoint, toPoint: lastPoint)
+        }
+    }
+
     func chatToggleFn() { //function called to toggle the chat view
         let windowWidth = self.drawView.frame.width
         let chatViewWidth = self.chatView.frame.width
