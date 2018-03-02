25cb2b98786a73222b3048dd1f6e1018ee9581a9
Gabriel Bourgault <gabriel.bourgault@polymtl.ca>
2018-03-02 17:55:10 -0500

    refs #16653 [serveur] Add a helper ProcessTimer

---
+++ serveur/src/helpers/process-timer.ts
@@ -0,0 +1,33 @@
+//Inspired by https://stackoverflow.com/a/14551263/6316091
+export class ProcessTimer {
+    private static readonly Precision = 3;
+    private startTime: [number, number];
+    private elapsedTime: number;
+
+    public start(): void {
+        this.startTime = process.hrtime();
+    }
+
+    public stop(): void {
+        this.elapsedTime = process.hrtime(this.startTime)[1] / 1000000; // divide by a million to get nano to milli
+    }
+
+    /**
+     * Returns a string with the elapsed time in ms
+     * @param {number} precision
+     * @returns {string} elapsed time in ms
+     */
+    public getElapsedTime(precision: number = ProcessTimer.Precision): string {
+        return this.elapsedTime.toFixed(precision);
+    }
+
+    /**
+     * Prints the time taken by the operation, only if DEBUG is active
+     */
+    public print(operation?: string): void {
+        if (process.env.DEBUG) {
+            operation = (operation) ? "- " + operation : "";
+            console.log(`Operations took ${this.getElapsedTime()} ms ${operation}`);
+        }
+    }
+}
