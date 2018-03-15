cec8788beb39c62485037cbfac7104fc06db2675
Gabriel Bourgault <gabriel.bourgault@polymtl.ca>
2018-03-14 23:31:11 -0400

    refs #17196 [leger] Notifications binded with incoming messages

--- client-leger/PolyPaintPro/PolyPaintPro/Controller/EditorViewController.swift
+++ client-leger/PolyPaintPro/PolyPaintPro/Controller/EditorViewController.swift
@@ -8,8 +8,10 @@
 
 import Foundation
 import SpriteKit
+import AVFoundation
 
 class EditorViewController: UIViewController, SocketManagerDelegate {
+    private var timer: Timer!
     internal var chatShowing = false
     internal var toolsShowing = false
     internal var drawingSettingsShowing = false
@@ -85,6 +87,7 @@ class EditorViewController: UIViewController, SocketManagerDelegate {
             chatViewConstraint.constant = windowWidth - chatViewWidth
         }
         UIView.animate(withDuration: 0.3, animations: {self.view.layoutIfNeeded()})
+        self.chatToggleBtn.tintColor = UIColor.blue
         chatShowing = !chatShowing
     }
 
@@ -97,6 +100,18 @@ class EditorViewController: UIViewController, SocketManagerDelegate {
         }
         UIView.animate(withDuration: 0.3, animations: {self.view.layoutIfNeeded()})
         drawingSettingsShowing = !drawingSettingsShowing
+    }
+
+    private func notifyMessage(message: String, messageInfos: (author: String, timestamp: String)) {
+        self.timer?.invalidate()
+        self.chatToggleBtn.tintColor = UIColor.red
+        if self.chatShowing {
+            self.timer = setTimeout(0.5) { //Flash red for 1 second if chat is showing
+                self.chatToggleBtn.tintColor = UIColor.blue
+            }
+        }
+
+        AudioServicesPlaySystemSound(SystemSoundID(1003)) //See https://github.com/TUNER88/iOSSystemSoundsLibrary
     }
 
     internal func connect() {
@@ -117,6 +132,9 @@ class EditorViewController: UIViewController, SocketManagerDelegate {
             let timestamp = convertTime.getTimeFromServer(timestamp: incomingMessage.timestamp)
             let messageInfos = (incomingMessage.author.username, timestamp)
             chatView.displayMessage(message: incomingMessage.message, messageInfos: messageInfos)
+            if incomingMessage.author.username != AccountManager.sharedInstance.username {
+                self.notifyMessage(message: incomingMessage.message, messageInfos: messageInfos)
+            }
         } catch let error {
             print(error)
         }
---
+++ client-leger/PolyPaintPro/PolyPaintPro/Helpers/Timers.swift
@@ -0,0 +1,47 @@
+//
+//  Timers.swift
+//  PolyPaintPro
+//
+//  Created by Gabriel Bourgault on 2018-03-14.
+//  Copyright © 2018 Les Pods c'est pour les lunchs. All rights reserved.
+//
+
+import Foundation
+
+// See : https://gist.github.com/peatiscoding/4f8699d8acd635746037b844f0730336
+
+/**
+ setTimeout()
+
+ Shorthand method for create a delayed block to be execute on started Thread.
+
+ This method returns ``Timer`` instance, so that user may execute the block
+ within immediately or keep the reference for further cancelation by calling
+ ``Timer.invalidate()``
+
+ Example:
+ let timer = setTimeout(0.3) {
+ // do something
+ }
+ timer.invalidate()      // cancel it.
+ */
+func setTimeout(_ delay:TimeInterval, block:@escaping ()->Void) -> Timer {
+    return Timer.scheduledTimer(timeInterval: delay, target: BlockOperation(block: block), selector: #selector(Operation.main), userInfo: nil, repeats: false)
+}
+
+/**
+ setInternval()
+
+ Similar to setTimeout() this method will return ``Timer`` instance however, this
+ method will execute repeatedly.
+
+ Warning using this method with caution, it is recommended that the block to utilise
+ this method should call [unowned self] or [weak self] to announce OS that it should not
+ hold strong reference to this block.
+
+ In addition, ``Timer`` returned should kept as member variable, and call invalidated()
+ when the block no longer required. such as deinit, or viewDidDisappear()
+ */
+func setInterval(_ interval:TimeInterval, block:@escaping ()->Void) -> Timer {
+    return Timer.scheduledTimer(timeInterval: interval, target: BlockOperation(block: block), selector: #selector(Operation.main), userInfo: nil, repeats: true)
+}
