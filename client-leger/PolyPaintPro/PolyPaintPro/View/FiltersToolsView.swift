14102d6d430bf31f3017d423d58dd939191c7d0a
Frederic Halle <frederic.halle@polymtl.ca>
2018-04-12 00:27:00 -0400

    [leger] added the new uiview for the filters settings

---
+++ client-leger/PolyPaintPro/PolyPaintPro/View/FiltersToolsView.swift
@@ -0,0 +1,17 @@
+//
+//  FiltersToolsView.swift
+//  PolyPaintPro
+//
+//  Created by Fred Habsfan on 2018-04-12.
+//  Copyright © 2018 Les Pods c'est pour les lunchs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class FilterToolsView: UIView {
+    //weak var delegate: DrawingToolsViewDelegate?
+
+}
+
